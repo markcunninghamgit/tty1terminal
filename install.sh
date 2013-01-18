2b28fd213ebf50b81c04ffda0100151d726b4804
Mark Cunningham <contact@markcunningham.ie>
2013-01-18 20:00:59 +0000

    added ip to PS1

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+apt-get install openssh-server byobu vim less git
+echo syntax on > /root/.vimrc
+rm /etc/udev/rules.d/70-persistent-net.rules
+echo usedns no >> /etc/ssh/sshd_config
+/etc/init.d/ssh restart
 cp inittab /etc/inittab
 cp quickshell /bin/quickshell
+cp ip_prompt /usr/local/bin/ip_prompt
+cp bash.bashrc /etc/bash.bashrc
